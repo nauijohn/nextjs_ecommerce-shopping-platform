528cdf5c53ddf261fc6f40e1131f37714c26a9d5
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-03-14 22:21:57 +0800

    Section 2: Chapter 11. Header & Footer Components

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,43 @@
+import { ShoppingCart, UserIcon } from "lucide-react";
+
+import { APP_NAME } from "@/lib/constants";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="border-b w-full">
+      <div className="flex-between wrapper">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo`}
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block ml-3 font-bold text-2xl">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href="/cart">
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href="/sign-in">
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
