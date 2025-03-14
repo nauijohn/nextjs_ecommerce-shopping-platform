cace4c0e955a40632d54f49c55908948830d5ea7
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-03-14 21:20:55 +0800

    Section 2: ShadCN UI Setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "@/assets/styles/globals.css";
+
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Prostore",
+  description: "A modern ecommerce platform built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
