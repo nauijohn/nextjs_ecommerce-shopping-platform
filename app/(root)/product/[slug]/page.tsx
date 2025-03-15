6e1131499533bcee7f70fd693f075d203678286e
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-03-15 22:32:53 +0800

    Section 3: Chapter 25. Product Details Page

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,77 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import ProductPrice from "@/components/shared/product/product-price";
+import { getProductBySlug } from "@/lib/actions/product.actions";
+import { notFound } from "next/navigation";
+
+const ProductDetailsPage = async (props: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await props.params;
+
+  const product = await getProductBySlug(slug);
+  if (!product) notFound();
+
+  return (
+    <>
+      <section>
+        <div className="grid grid-cols-1 md:grid-cols-5">
+          {/** Images Column */}
+          <div className="col-span-2">{/** Images Component */}</div>
+          {/** Details Column */}
+          <div className="col-span-2 p-5">
+            <div className="flex flex-col gap-6">
+              <p>
+                {product.brand} {product.category}
+              </p>
+              <h1 className="h3-bold">{product.name}</h1>
+              <p>
+                {product.rating} of {product.numReviews} Reviews
+              </p>
+              <div className="flex sm:flex-row flex-col sm:items-center gap-3">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="bg-green-100 px-5 py-2 rounded-full w-24 text-green-700"
+                />
+              </div>
+            </div>
+            <div className="mt-10">
+              <p className="font-semibold">Description</p>
+              <p>{product.description}</p>
+            </div>
+          </div>
+          {/** Action Column */}
+          <div>
+            <Card>
+              <CardContent className="p-4">
+                <div className="flex justify-between mb-2">
+                  <div>Price</div>
+                  <div>
+                    <ProductPrice value={Number(product.price)} />
+                  </div>
+                </div>
+                <div className="flex justify-between mb-2">
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge variant="outline">In Stock</Badge>
+                  ) : (
+                    <Badge variant="destructive">Out Of Stock</Badge>
+                  )}
+                </div>
+                {product.stock > 0 && (
+                  <div className="flex-center">
+                    <Button className="w-full">Add To Cart</Button>
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ProductDetailsPage;
